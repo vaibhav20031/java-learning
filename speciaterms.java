542551afe742875e371fc14b1fca12d37b3fa56a
vaibhav20031 <vaibhavastro2003@gmail.com>
2023-04-14 16:51:20 +0530

    new files

---
+++ speciaterms.java
@@ -0,0 +1,15 @@
+// super()
+// final() 
+public class speciaterms{
+    int a =10;
+    String name ="vaibhav";
+    public speciaterms(int data){
+        a=data;
+        System.out.println("jai shree krishna ");
+        System.out.println(a);
+    }
+    public static void main(String[] args){
+        speciaterms obj=new speciaterms(100);
+        // System.out.println(obj.name);
+    }
+}
